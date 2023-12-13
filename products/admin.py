aeb70ed1e66ddc7b46ff856f18d3049bd6ccc8d4
karim tarek <142750275+karimt201@users.noreply.github.com>
2023-12-13 03:05:36 +0300

    add models in admin ,add tabular inline

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product , Brand , ProductImages , Review
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImages
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline ]
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Brand)
+admin.site.register(Review)
+
 
 # Register your models here.
--- products/models.py
+++ products/models.py
@@ -60,7 +60,7 @@ class Review(models.Model):
     user = models.ForeignKey(User,verbose_name=_('user'),related_name='review_user',on_delete=models.SET_NULL,null=True)
     product = models.ForeignKey(Product,verbose_name=_('product'),related_name='review_product',on_delete=models.CASCADE)
     review = models.TextField(_('review'),max_length=500)
-    rate = models.IntegerChoices(_('rate'),choices = [(i,i) for i in range(1,6)])
+    rate = models.IntegerField(_('rate'),choices = [(i, i) for i in range(1, 6)])
     create_at = models.DateTimeField(_('create_at'),default=timezone.now) 
 
     def __str__(self) :
