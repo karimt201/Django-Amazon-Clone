40bead724b124bbadaf0b10c534f6e065cb715ce
karim tarek <142750275+karimt201@users.noreply.github.com>
2023-12-15 12:03:44 +0300

    show product list & detail in temolate , review in template

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView , DetailView
-from .models import Product , Brand , review
+from .models import Product , Brand 
 
 
 class ProductList(ListView):
